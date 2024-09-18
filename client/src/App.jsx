f184bdaa012e5bd156c2c0e3417544deb2dfd092
Carmen <carmencheney99@gmail.com>
2024-09-18 13:03:32 -0400

    package.json and app.jsx updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {ProfileBlock} from './components/profile-block/ProfileBlock'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <ProfileBlock />
+    <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
