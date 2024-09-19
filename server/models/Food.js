b4a735899e41a0226e445f1148aed35333bf5aab
Al Bundy <gale.s.salazar22@gmail.com>
2024-09-19 15:30:13 -0500

    added food.js schema

---
+++ server/models/Food.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const foodSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        location: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+const Food = model('Food', foodSchema);
+
+module.exports = Food;
+
+// referenced 21-MERN ACIVITY 9 MODELS SCHOOL.JS
